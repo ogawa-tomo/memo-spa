d05d695acb736a55bf68b4049934a45a28f3510e
tomo <o_tomo_1990@yahoo.co.jp>
2023-04-02 19:54:33 +0900

    データをLocalStorageに保存する

--- src/stores/memos.js
+++ src/stores/memos.js
@@ -13,7 +13,15 @@ export const useMemosStore = defineStore('memos', () => {
       return this.content.split('\n')[0]
     }
   }
-  const memos = ref([new Memo(1, 'hogehoge'), new Memo(2, 'fugafuga')])
+  const loadMemosData = () => {
+    const memos_data = localStorage.getItem('memos')
+    if (memos_data) {
+      return JSON.parse(memos_data).map((memo) => new Memo(memo.id, memo.content))
+    } else {
+      return []
+    }
+  }
+  const memos = ref(loadMemosData())
   const update = (id, content) => {
     if (id) {
       const memo = getById(id)
@@ -23,20 +31,25 @@ export const useMemosStore = defineStore('memos', () => {
       const id = memos.value.length === 0 ? 1 : Math.max(...memos.value.map((memo) => memo.id)) + 1
       memos.value.push(new Memo(id, content))
     }
+    saveMemosData()
   }
   const remove = (id) => {
     const memo = getById(id)
     const idx = memos.value.indexOf(memo)
     memos.value.splice(idx, 1)
+    saveMemosData()
   }
   const getById = (id) => {
     return memos.value.find((memo) => memo.id === id)
   }
-
   const finishEdit = () => {
     for (const memo of memos.value) {
       memo.editing = false
     }
+    saveMemosData()
+  }
+  const saveMemosData = () => {
+    localStorage.setItem('memos', JSON.stringify(memos.value))
   }
 
   return { memos, update, remove, finishEdit, getById }
